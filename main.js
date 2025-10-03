f8be797feabc2ae5d07b2689b2576db13f571b44
Isadora Aguirre Dal Conte <isaguirredc@gmail.com>
2025-10-03 01:20:19 -0300

    versão final

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ musicBtn.addEventListener("click", () => {
         // muda o ícone para volume-off
         musicBtn.innerHTML = '<i data-lucide="volume-off"></i>';
     }
-    // atualiza os ícones do Lucide
+    // atualiza os ícones do lucide
     lucide.createIcons();
 });
 
@@ -45,3 +45,34 @@ document.addEventListener('mousemove', e => {
     cursor.style.top = e.clientY + 'px';
     cursor.style.left = e.clientX + 'px';
 });
+
+const starsContainer = document.querySelector('.stars-container');
+
+for (let i = 0; i < 150; i++) { // AJUSTAR QUANTIDADE 
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.top = Math.random() * 100 + '%';
+    star.style.left = Math.random() * 100 + '%';
+    star.style.width = star.style.height = Math.random() * 6 + 2 + 'px'; // AJUSTAR TAMANHO
+    star.style.animationDuration = (Math.random() * 2 + 1) + 's';
+    starsContainer.appendChild(star);
+    star.style.boxShadow = "0 0 18px white, 0 0 14px #bbf2dc";
+
+}
+
+const navbar = document.querySelector('.navbar');
+const hero = document.querySelector('.hero');
+
+function updateNavbarIcons() {
+    const heroBottom = hero.getBoundingClientRect().bottom;
+    if (heroBottom > 0) {
+        // ainda no hero
+        navbar.classList.add('scrolled');
+    } else {
+        // saiu do hero
+        navbar.classList.remove('scrolled');
+    }
+}
+
+window.addEventListener('scroll', updateNavbarIcons);
+window.addEventListener('load', updateNavbarIcons);
